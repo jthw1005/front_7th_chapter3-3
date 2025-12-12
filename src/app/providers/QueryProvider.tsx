7174d6c1a3d99ec3aa2f91b383575caf738879c8
jthw1005 <hjt105@gmail.com>
2025-12-13 02:45:37 +0900

    chore: tanstack query dev tools 적용

--- src/app/providers/QueryProvider.tsx
+++ src/app/providers/QueryProvider.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { ReactNode, useState } from "react"
 
 interface QueryProviderProps {
@@ -19,5 +20,10 @@ export const QueryProvider = ({ children }: QueryProviderProps) => {
       }),
   )
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
