c3e34e0fdd62bf56090694e171a1d6f3b44decb2
jthw1005 <hjt105@gmail.com>
2025-12-12 15:58:39 +0900

    chore: 절대 경로 설정

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,15 @@
-export { Button } from "./Button"
-export { Input } from "./Input"
-export { Card, CardHeader, CardTitle, CardContent } from "./Card"
-export { Textarea } from "./Textarea"
-export { Select, SelectGroup, SelectValue, SelectTrigger, SelectContent, SelectItem } from "./Select"
-export { Dialog, DialogTrigger, DialogPortal, DialogOverlay, DialogContent, DialogHeader, DialogTitle } from "./Dialog"
-export { Table, TableHeader, TableBody, TableRow, TableHead, TableCell } from "./Table"
+export { Button } from "@/components/Button"
+export { Input } from "@/components/Input"
+export { Card, CardHeader, CardTitle, CardContent } from "@/components/Card"
+export { Textarea } from "@/components/Textarea"
+export { Select, SelectGroup, SelectValue, SelectTrigger, SelectContent, SelectItem } from "@/components/Select"
+export {
+  Dialog,
+  DialogTrigger,
+  DialogPortal,
+  DialogOverlay,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/Dialog"
+export { Table, TableHeader, TableBody, TableRow, TableHead, TableCell } from "@/components/Table"
